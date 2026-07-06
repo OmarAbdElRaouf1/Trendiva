63513c1d40fec964eb093c3695ab1187c24f0fca
MohamedElHussein12 <mohussein539@gmail.com>
2026-07-07 01:58:45 +0300

    add otp screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
 import 'package:nti_final_project/core/routing/app_router.dart';
 import 'package:nti_final_project/core/routing/routes.dart';
 import 'package:nti_final_project/features/auth/presentation/views/forget_password.dart';
+import 'package:nti_final_project/features/auth/presentation/views/otp_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Trendiva',
         // initialRoute: Routes.loginView,
-        home:  ForgotPasswordScreen(),
+        home:  OtpVerificationScreen(),
         onGenerateRoute: AppRouter.onGenerateRoute,
       ),
     );
