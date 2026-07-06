78c9d52c65be089e67864342151fee1a09ac2bbc
OmarAbdElRaouf1 <omarahmedabdelraouf9@gmail.com>
2026-07-06 14:49:26 +0300

    -implements Product Details UI -refactor all code -add state management (Cubit)

---
+++ lib/features/home/presentation/cubit/products_state.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:nti_final_project/features/home/data/models/product_model.dart';
+
+sealed class ProductsState extends Equatable {
+  const ProductsState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ProductsInitial extends ProductsState {
+  const ProductsInitial();
+}
+
+class ProductsLoading extends ProductsState {
+  const ProductsLoading();
+}
+
+class ProductsLoaded extends ProductsState {
+  const ProductsLoaded(this.products);
+
+  final List<ProductModel> products;
+
+  @override
+  List<Object?> get props => [products];
+}
+
+class ProductsError extends ProductsState {
+  const ProductsError(this.message);
+
+  final String message;
+
+  @override
+  List<Object?> get props => [message];
+}
